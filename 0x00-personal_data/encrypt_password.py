8d834e04854edf15effe82aec0e2927ec50faa69
Johnson adeola <johnsamxy@gmail.com>
2024-04-11 12:04:24 +0100

    new task

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+a hash_password function to return a hashed password.
+"""
+import bcrypt
+from bcrypt import hashpw
+
+
+def hash_password(password: str) -> bytes:
+    """
+        Returns a hashed password
+    """
+    b = password.encode()
+    hashed = hashpw(b, bcrypt.gensalt())
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check whether a password is valid
+    Args:
+        hashed_password (bytes): hashed password
+        password (str): password in string
+    Return:
+        bool
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
