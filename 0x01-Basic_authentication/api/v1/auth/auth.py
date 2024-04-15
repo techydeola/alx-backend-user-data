82d8ab8255a79f0c29d940f1f70432df4e4c9b12
Johnson adeola <johnsamxy@gmail.com>
2024-04-16 00:11:26 +0100

    new task

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" a module for handling authentication
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """A class for authentication
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """A public method
+            Returns: False
+        """
+
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ A public method
+            Returns: None
+        """
+
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Flask request object method
+        """
+
+        return None
