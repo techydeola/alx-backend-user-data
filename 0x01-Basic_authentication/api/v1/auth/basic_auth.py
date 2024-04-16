4a8288d80649c3764fbdf69f3c225d048c8d4de2
Johnson adeola <johnsamxy@gmail.com>
2024-04-16 19:19:10 +0100

    new task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,9 +3,21 @@
 """
 
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """ a basic authentication class
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ enconcodes the authorization header
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header.split(" ")[1]
