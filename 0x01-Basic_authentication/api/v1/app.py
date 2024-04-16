e36cd129679e84aa0958661b4da5fc495ac11bf3
Johnson adeola <johnsamxy@gmail.com>
2024-04-16 18:50:15 +0100

    new task

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -17,6 +17,9 @@ auth = None
 if os.environ.get("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+if os.environ.get("AUTH_TYPE") == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" a module that for basic authentication
+"""
+
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """ a basic authentication class
+    """
+    pass
