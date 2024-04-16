4fa779290230b213aea11a35483ea9de38ef9360
Johnson adeola <johnsamxy@gmail.com>
2024-04-16 19:41:17 +0100

    new task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -36,3 +36,17 @@ class BasicAuth(Auth):
             return raw_data
         except Exception as e:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ returns the user email and password
+            from the Base64 decoded value.
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+
+        return tuple(decoded_base64_authorization_header.split(":"))
