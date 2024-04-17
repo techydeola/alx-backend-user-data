3beec47a5015ddf3cd28ce8c47aff05acc4f5b6f
Johnson adeola <johnsamxy@gmail.com>
2024-04-17 14:02:27 +0100

    new task

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,22 @@
 """
 
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ a session authentication class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ A function that creates a session id
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
