473e097421c02c8b59802e0add11692c5232f23a
Johnson adeola <johnsamxy@gmail.com>
2024-04-17 14:46:47 +0100

    new task

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -18,7 +18,17 @@ class SessionAuth(Auth):
             return None
         if not isinstance(user_id, str):
             return None
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ a method that returns user id based on session id
+        """
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
