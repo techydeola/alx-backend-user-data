29e8ece08235ba3d9e8b356b7e70fd205aba4b88
Johnson adeola <johnsamxy@gmail.com>
2024-04-17 13:44:55 +0100

    new task

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" a module for authentication mechanism
+"""
+
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """ a session authentication class
+    """
+    pass
